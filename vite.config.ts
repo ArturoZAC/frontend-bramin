afa4e0a6404126693c6ce9d17d8a4a89c3dcdcf6
4z4c <arturoyz2105@gmail.com>
2025-11-07 11:30:33 -0500

    resetRoutes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing_page/plantilla9/'
-})
+  base: "/landingpages/bramin/",
+  // base: '/landing_page/plantilla9/'
+});
